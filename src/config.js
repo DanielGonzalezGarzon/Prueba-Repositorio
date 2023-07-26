6f0d7fa67dacb1d1671d4854c7f354a273f87316
DanielGonzalezGarzon <daniel.gonzalez.g.14@gmail.com>
2023-07-25 23:18:42 -0500

    first comit practis nodeJsWEb

--- src/config.js
+++ src/config.js
@@ -1,4 +1,27 @@
-export const PORT = process.env.PORT || 3000
+/* export const PORT = process.env.PORT || 3000 */
+
+let numbers = [1,2,3]/[3,2,1]
+/* const numbers = (await readline.next()).value
 
 
+const init = {input: process.stdin, output: process.stdout}
+const readline = require('readline').createInterface(init)[Symbol.asyncIterator]();
+
+
+(async () =>{
+    let line1 =(await readline.next()).value 
+    let line2 =(await readline.next()).value
+
+    console.log(line1)
+    console.log(line2)
+})(); */
+
+console.log(numbers)
+
+
+/* const ovejas ={
+    'Noah': azul,
+    'Euge': roja,
+    'Ki Na Ma': roja
+} */
  
